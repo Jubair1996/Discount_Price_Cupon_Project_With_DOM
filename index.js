38fb447d6471b9c572686669706b6ef693958998
Jubari1996 <abujubari1996@gmail.com>
2023-08-17 00:58:24 +0600

    js file added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ document.getElementById("apply-btn").addEventListener("click", function () {
   const discountInputValue = getInputValueById("discount-field");
   const couponField = document.getElementById("coupon-field");
   const couponFieldValue = couponField.value;
-  if (couponFieldValue === "DISC30") {
+  if (couponFieldValue === "DISCOUNT") {
     const discountPrice = (originalPrice * discountInputValue) / 100;
     const discountPriceResults = originalPrice - discountPrice;
     const payAmount = document.getElementById("pay-amount");
